9f15348dbfe5d9167281ecc2be46a0431eeb0cd3
Nikhil Dabhade <nikhild12377@gmail.com>
2026-07-16 18:42:16 +0530

    fix: updated the sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,12 +1,22 @@
-import { MetadataRoute } from 'next'
+import { MetadataRoute } from "next";
+import { blogs } from "./blogs/constants";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: 'https://nikhildabhade.com',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 1,
-    },
-  ]
+return [
+  {
+    url: "https://nikhildabhade.com",
+    lastModified: new Date(),
+    priority: 1,
+  },
+  {
+    url: "https://nikhildabhade.com/blogs",
+    lastModified: new Date(),
+    priority: 0.9,
+  },
+  ...blogs.map((blog) => ({
+    url: `https://nikhildabhade.com/blogs/${blog.slug}`,
+    lastModified: new Date(blog.date),
+    priority: 0.8,
+  })),
+];
 }
