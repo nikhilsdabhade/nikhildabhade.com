70b98e1ceed156711edf53b9ceb46f5941cc5502
Nikhil Dabhade <nikhild12377@gmail.com>
2026-07-17 09:42:45 +0530

    fix: added dynamic year in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className="w-full py-8 border-t border-neutral-200 dark:border-[#2A2A2A]/50 bg-white/70 dark:bg-[#0a0a0a]/70 backdrop-blur-md">
       <div className="container mx-auto px-4 flex flex-col justify-left gap-2 text-right text-sm md:text-base text-neutral-500 dark:text-white/50">
-        <p>&copy; 2026 Nikhil Dabhade</p>
+        <p>&copy; {currentYear} Nikhil Dabhade</p>
       </div>
     </footer>
   );
